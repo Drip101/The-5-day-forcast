57245b7962e39fa0927c1a952ba19c1534ede0a4
Maddrick Long <110777599+Drip101@users.noreply.github.com>
2022-10-04 18:30:03 -0400

    added buttons for city search, and started API

--- script.js
+++ script.js
@@ -18,14 +18,20 @@ console.log(localStorage.getItem('city'))*/
 
 // http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid={API key}
 
-let userInput=document.querySelector("input")
-let searchbtn=document.getElementById("search")
-let history=document.getElementById("history")
-searchbtn.addEventListener("click", ()=>{
-createbtn()
+let userInput = document.querySelector("input")
+let searchbtn = document.getElementById("search")
+let history = document.getElementById("history")
+//https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
+const API = "00155ff66eca5e916eeb91e633dbb8f0"
+searchbtn.addEventListener("click", () => {
+    createbtn()
+    getcity(userInput.value)
 })
-function createbtn(){
-let newbtn=document.createElement("button")
-newbtn.innerText=userInput.value
-history.appendChild(newbtn)
+function createbtn() {
+    let newbtn = document.createElement("button")
+    newbtn.innerText = userInput.value
+    history.appendChild(newbtn)
+}
+function getcity(city) {
+    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${API}`).then(response => response.json()).then(a => console.log(a))
 }
